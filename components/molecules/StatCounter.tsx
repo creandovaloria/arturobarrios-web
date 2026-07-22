ff2f082c277cca67f62c82f2d6293c4622fadee2
Arturo Barrios <creandovalor.ia@gmail.com>
2026-07-21 20:25:51 -0600

    fix: remove unused useState import

--- components/molecules/StatCounter.tsx
+++ components/molecules/StatCounter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { useCountUp, useInViewAnimation, useReducedMotion } from '@hooks';
 import { durations, easings } from '@lib/motion';
